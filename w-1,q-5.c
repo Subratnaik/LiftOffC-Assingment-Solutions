80d7ad8fe147ed73e2aa7ce36c498d0b6137af48
Subratnaik <99497748+Subratnaik@users.noreply.github.com>
2022-02-12 22:47:58 +0530

    w-1,q-5

--- w-1,q-5.c
+++ w-1,q-5.c
@@ -4,11 +4,21 @@
 {
     float radius;
     float area;
+    float diameter;
+    float circumference;
+    float r,a,d,c;
     printf("Enter the radius of the circle\n");
-    scanf("%f",&radius);
+    scanf("%f",&r);
     
-    area = M_PI*radius*radius;
+    area = M_PI*r*r;
     
-    printf("Area of the circle=%f",area);
+    diameter = 2*r;
+    
+    circumference = 2*M_PI*r;
+    
+    printf("Area of the circle=%f \n",area);
+    printf("Diameter is=%f \n", diameter);
+    printf("Circumference is %f", circumference);
     return 0;
+    
 }
