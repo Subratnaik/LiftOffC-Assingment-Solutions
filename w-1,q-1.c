a63dbb0153954ac541f633218d2bbff6d0fd96f0
Subratnaik <99497748+Subratnaik@users.noreply.github.com>
2022-02-12 22:42:27 +0530

    w-1,q-1

--- w-1,q-1.c
+++ w-1,q-1.c
@@ -1,10 +1,21 @@
-#include<stdio.h>
-
-int main()
+#include<stdio.h>  
+void main()  
 {
-    printf("Name:- Manoranjan Naik\n");
-    printf("Regd no.:-115\n");
-    printf("Branch:- Electrical engineering\n");
-    printf("Hobbies:- Playing video games,playing badminton.\n");
-    return 0;
+  char name[20];  
+  int reg[3];
+  char branch[30];
+  char hobbies[40];
+
+  printf("Enter Your Name : "); 
+  scanf("%s",&name);  
+  printf("Enter your registration no:");
+  scanf("%s",&reg);
+  printf("Enter your branch: ");
+  scanf("%s",&branch);
+  printf("Enter your hobbies: ");
+  scanf("%s",&hobbies);
+  printf("Name : %s \n", name);  
+  printf("Registration no: %s \n", reg);
+  printf("Branch: %s \n", branch);
+  printf("Hobbies: %s \n", hobbies);
 }
